ad266e464acd765381d3e8be105c61ba066c2074
Akun <2099940928@qq.com>
2022-07-09 10:34:27 +0800

    new

--- src/leetcode/text.java
+++
@@ -1,9 +0,0 @@
-package leetcode;
-
-public class text {
-    public static void main(String[] args) {
-        int[][] a = {{1, 2}, {5, 6}, {6, 8}, {5, 9}, {2, 6}};
-        countGoodRectangles b = new countGoodRectangles();
-        System.out.println(b.countGoodRectangles(a));
-    }
-}
